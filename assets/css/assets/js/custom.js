df7f50117fa3c3b966c952fb5051d42b8179b6d9
Shreya0903 <83579397+Shreya0903@users.noreply.github.com>
2025-09-17 15:10:10 -0400

    Delete assets/css directory

--- assets/css/assets/js/custom.js
+++
@@ -1,5 +0,0 @@
-const io=new IntersectionObserver(
-  es=>es.forEach(e=>{ if(e.isIntersecting) e.target.classList.add('visible') }),
-  {threshold:.15}
-);
-document.querySelectorAll('.reveal').forEach(el=>io.observe(el));
